bd00def6ef6b9c0340fc17664d036dc6699f10ec
meijieman <1558667079@qq.com>
2017-12-18 01:26:12 +0800

    设置 alias 成功后回调 并通知 具有相同 alias 的其他业务帐号

--- Androidpn-tomcat/src/org/androidpn/server/xmpp/handler/IQSetAliasHandler.java
+++ Androidpn-tomcat/src/org/androidpn/server/xmpp/handler/IQSetAliasHandler.java
@@ -18,7 +18,18 @@ import org.xmpp.packet.PacketError;
 import java.util.List;
 
 /**
- * 设置别名，一个名字对应一个别名，别名不能重复
+ * 别名用于绑定设备号和业务帐号
+ * <p>
+ * 对于一个 App 而言（包名）
+ * 一个设备号只能对应一个别名
+ * 一个别名可以对应多个设备号（多终端）
+ * <p>
+ * （如果一个设备上存在多个推送 sdk，则会出现一个设备号对应多个别名，但这个别名属于不同的 APP（包名）
+ * <p>
+ * 1. 当只允许一个终端登录业务帐号时，推送服务端在设置别名时需要判断是否已存在与该别名绑定的设备，
+ * 存在则需要推送消息到之前登录的客户端，让其主动退出登录，并告知业务服务端清除之前登录的 token
+ * 2. 当允许多终端登录业务帐号时，，推送服务端在设置别名时需要判断是否已存在与该别名绑定的设备，
+ * 存在则需要推送消息到之前登录的客户端，让其提示其他终端登录
  */
 public class IQSetAliasHandler extends IQHandler {
 
@@ -53,25 +64,43 @@ public class IQSetAliasHandler extends IQHandler {
                 Element element = packet.getChildElement();
 //                String username = element.elementText("username");
                 String alias = element.elementText("alias");
-//                if (/*CommonUtil.isNotEmpty(username) && */CommonUtil.isNotEmpty(alias)) {
-                    try {
+                User user = null;
+                try {
+                    user = userService.getUserByUsername(session.getUsername());
+                    if (CommonUtil.isNotEmpty(user.getAlias()) && user.getAlias().equalsIgnoreCase(alias)) {
+                        log.warn("already set alias " + alias + "to " + user.getUsername());
+                    } else {
+                        user.setAlias(alias);
+                        userService.saveUser(user);
+                        log.info("set alias " + alias + "to " + user.getUsername() + " success");
+                    }
+                    // 给当前用户发送一个设置成功穿透消息
+                    String message = "用户" + user.getUsername() + "设置别名" + alias + "成功";
+                    notificationManager.sendNotifcationToUser("", user.getUsername(), "", message, "", 0, "[username]" + user.getUsername(), "payload", true);
+
+                    // alias 为空表示取消设置 alias（业务帐号退出登录），不需要发送消息
+                    if (CommonUtil.isNotEmpty(alias)) {
                         List<User> users = userService.getUsersByAlias(alias);
                         if (CommonUtil.isNotEmpty(users)) {
                             log.info("already exist " + users + " count bind alias " + alias);
-                            // 已经有其他设备绑定了该 alias（已用业务帐号登录了），需要通知其他设备有新的绑定（当前有新设备被登录业务帐号），以让其他设备做处理（下线）
-                            // TODO: 2017/11/16
-//                            notificationManager.send
-                        } else {
-                            User user = userService.getUserByUsername(session.getUsername());
-                            user.setAlias(alias);
-                            userService.saveUser(user);
-                            log.info("set alias " + alias + "to " + user.getUsername() + " success");
+                            // 已经有其他设备绑定了该 alias（已用业务帐号登录了），需要通知其他设备有新的绑定（当前有新设备被登录业务帐号），以让其他设备做处理（下线，提示新设备登录）
+                            String message1 = "新增用户" + user.getUsername() + "设置别名" + alias + "，当前用户需要提示或下线";
+                            for (User user1 : users) {
+                                // 排除当前 alias
+                                if (!user.getUsername().equalsIgnoreCase(user1.getUsername())) {
+                                    notificationManager.sendNotifcationToUser("", user1.getUsername(), "", message1, "", 0, "[username]" + user1.getUsername(), "payload", true);
+                                }
+                            }
                         }
-                    } catch (UserNotFoundException | UserExistsException e) {
-//                        e.printStackTrace();
-                        log.error("set alias error " + e);
                     }
-//                }
+                } catch (UserNotFoundException | UserExistsException e) {
+                    log.error("设置 alias 失败 " + e);
+                    // 给当前用户发送一个设置失败穿透消息
+                    if (user != null) {
+                        String message = "用户" + user.getUsername() + "设置别名" + alias + "失败";
+                        notificationManager.sendNotifcationToUser("", user.getUsername(), "", message, "", 0, "[username]", "payload", true);
+                    }
+                }
             }
         }
         return null;
